b3048319a179a9361144deac7324a76b9ff1bc5c
Yamil Gonzales <yamil@live.com>
2010-12-06 02:26:45 +0800

    version final por ahora, voy a comenzar a usarla para ver que le falta

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $sqlquery = "INSERT INTO citas VALUES('$cita','$autor','$fuente','$id')";
 // El if es para evitar que se los datos se manden vacios cuando se carge la pagina
 if (!empty($_POST['cita'])) {
    mysql_query($sqlquery);
-   echo "Los valores fueron introducidos.";
 }
 
 // la db ya esta seleccionado ahora se selecciona todo de la tabla
@@ -41,8 +40,17 @@ mysql_close();
     padding:0;
   }
   
-  header {
+  header,footer {
     display:block;
+  }
+  
+  a {
+    text-decoration:none;
+  }
+  
+  footer p {
+    text-align:center;
+    color:red;
   }
   
   body {
@@ -50,6 +58,15 @@ mysql_close();
     width:640px;
     font:13px/22px Cambria, Helvetica, Arial, sans-serif;
     color:#333;
+  }
+  
+  h1 {
+    font-size:2.5em;
+  }
+  
+  h1 {
+    text-align:center;
+   
   }
   
   #cita {
@@ -60,17 +77,34 @@ mysql_close();
     padding:5px;
     border-radius: 5px;
     margin:5px;
+    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, arial, sans-serif; 
+    font-weight: 300;
+    font-size:1.4em;
+    border: 1px solid #E4E4E4;
+  }
+  
+  form textarea {
+    margin:10px 0 12px 0;
+    padding:10px;
   }
   
   blockquote {
-    font-style:italic;
     font-family:Cambria, serif;
     font-size:1.5em;
     text-align:center;
+    color:#333;
+    line-height:1.4em;
+    margin-top:-20px;
   }
   
   #view-citas dl dd {
-    text-align:right;
+    text-align:center;
+    margin-bottom:-20px;
+  }
+  
+ 
+  form fieldset {
+    border:none;
   }
   
   hr {
@@ -92,7 +126,76 @@ mysql_close();
     );
     height: 1px;
     border:0;
+    margin:50px;
   }
+  
+  /* fuente del siguiente codigo: http://ubuwaits.github.com/css3-buttons/ junto con mis respetos*/
+    
+    #add-cita input[type='submit'] {
+      background: #29C636;
+      background: -moz-linear-gradient(0% 100% 90deg, #00890B 0%, #00B50F 50%, #29C636 50%, #2EEE3E 100%);
+      background: -webkit-gradient(linear, 0 0, 0 100%, color-stop(0, #2EEE3E), color-stop(0.5, #29C636), color-stop(0.5, #00B50F), color-stop(1, #00890B));
+      border: 1px solid #00950C;
+      -moz-border-radius: 5px;
+      -webkit-border-radius: 5px;
+      border-radius: 5px;
+      -moz-box-shadow: inset 0px 0px 0px 1px rgba(100, 255, 113, 0.4), 0 1px 3px #333;
+      -webkit-box-shadow: inset 0px 0px 0px 1px rgba(100, 255, 113, 0.4), 0 1px 3px #333;
+      box-shadow: inset 0px 0px 0px 1px rgba(100, 255, 113, 0.4), 0 1px 3px #333;
+      color: #fff;
+      font-size: 1.2em;
+      font-weight: bold;
+      letter-spacing: 1px;
+      line-height: 1em;
+      padding: 7px 0 8px 0;
+      text-align: center;
+      text-shadow: 0px -1px 1px rgba(0, 0, 0, .5);
+      width: 118px;
+    }
+
+    #add-cita input[type='submit']:hover {
+      background: #00CB11;
+      background: -moz-linear-gradient(0% 100% 90deg, #01A20E 0%, #00CB11 50%, #4DDB59 50%, #73F37E 100%);
+      background: -webkit-gradient(linear, 0 0, 0 100%, color-stop(0, #73F37E), color-stop(0.5, #4DDB59), color-stop(0.5, #00CB11), color-stop(1, #01A20E));
+    }
+
+    #add-cita input[type='submit']:active {
+      background: #00B30F;
+      background: -moz-linear-gradient(0% 100% 90deg, #007009 0%, #009C0D 50%, #24AD2F 50%, #28D436 100%);
+      background: -webkit-gradient(linear, 0 0, 0 100%, color-stop(0, #28D436), color-stop(0.5, #24AD2F), color-stop(0.5, #009C0D), color-stop(1, #007009));
+      -moz-box-shadow: inset 0px 0px 0px 1px rgba(255, 115, 100, 0.4);
+      -webkit-box-shadow: inset 0px 0px 0px 1px rgba(255, 115, 100, 0.4);
+      box-shadow: inset 0px 0px 0px 1px rgba(255, 115, 100, 0.4);
+    }
+    
+    /*sacado del CSS3 for Web Designers */
+    @-webkit-keyframes pulse {
+    	0% {
+    		-webkit-box-shadow: 0 0 12px rgba(51,204,255,.2);
+    	}
+    	50% {
+    		-webkit-box-shadow: 0 0 12px rgba(51,204,255,.9);
+    	}
+    	100% {
+    		-webkit-box-shadow: 0 0 12px rgba(51,204,255,.2);
+    	}
+    }
+
+    @-webkit-keyframes pulse-error {
+    	0% {
+    		-webkit-box-shadow: 0 0 12px rgba(204,0,0,.1);
+    	}
+    	50% {
+    		-webkit-box-shadow: 0 0 12px rgba(204,0,0,.5);
+    	}
+    	100% {
+    		-webkit-box-shadow: 0 0 12px rgba(204,0,0,.1);
+    	}
+    }
+
+    #add-cita fieldset input[type="text"]:focus {
+    	-webkit-animation: pulse 1.5s infinite ease-in-out;
+    	}
   
   </style>
 </head>
@@ -100,21 +203,20 @@ mysql_close();
 <body>
   
   <header>
-    <h1>Citas</h1>
+    <h1>&lt;Citas&gt;</h1>
   </header>
   
   <section id="add-form">
   <form id="add-cita" method="post" action="<? echo $PHP_SELF;?>">
   <input type="hidden" name="id" value="NULL">
     <fieldset>
-      <legend>Agregar nueva cita.</legend>
-    <label for="cita">Cita:</label>
-    <textarea id="cita" name="cita" required placeholder="Cita que quiero agregar..."></textarea>
+      <legend>Agregar cita:</legend>
+        <textarea id="cita" name="cita" required autofocus placeholder="Aqu&iacute; es donde se ingresan las citas"></textarea>
     <!-- agregar el autofocus luego -->
     <label for="autor">Autor:</label>
-    <input type="text" id="autor" name="autor" required placeholder="Juan Perez">
+    <input type="text" id="autor" name="autor" required placeholder="¿Qui&eacute;n lo escribi&oacute;?">
     <label for="fuente">Fuente:</label>
-    <input type="text" id="fuente" name="fuente" required placeholder="De donde lo saque">
+    <input type="text" id="fuente" name="fuente" placeholder="¿De d&oacute;nde lo saqu&eacute;?">
     <input type="submit" value="Agregar">
     </fieldset>
   </form>
@@ -122,10 +224,8 @@ mysql_close();
   </section>
   
   <section id="view-citas">
-    <header>
-      <h2>Colección:</h2>
-    </header>
-    <?  $i=0; while ($i < $num) {
+<hr>
+    <?  $i=$num-1; while ($i >= 0) {
       $mostrar_cita = mysql_result($resultados, $i, 'citas');
       $mostrar_autor = mysql_result($resultados, $i, 'autor');
       $mostrar_fuente = mysql_result($resultados, $i, 'fuente');
@@ -134,12 +234,15 @@ mysql_close();
               <dd><p>$mostrar_autor </p></dd>
               <dd><p>$mostrar_fuente </p></dd>
             <dl> <hr>";
-    $i++;
+    $i--;
     }
    
  ?>
   </section>
-  
+  <footer>
+    <p>tl;dr warning</p>
+    
+  </footer>
 </body>
 
 </html>
